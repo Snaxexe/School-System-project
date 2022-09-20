82fe19f0dbac008a16ce9c398496b50d40d13901
Snaxexe <kiwi0507@outlook.com>
2022-09-20 13:49:25 +0200

    added not working functions
    Will be looked into further
    
    Added functions:
    press 3 to search for students
    (In progress)
    
    added clears to menu to keep it clean
    (Still buggy)

--- Skola/src/SchoolSystem.cpp
+++ Skola/src/SchoolSystem.cpp
@@ -2,39 +2,58 @@
 #include <string>
 void SchoolSystem::Run()
 {
-	//Prints out start menu
-	std::cout << "School System" << "\n" << "To add a student, Press: 1" << "\n" << "To delete a student, Press: 2" << "\n";
-
-	//int to take the input to know where to go from the start menu
-	int input;
-	std::cin >> input;
-
-	//if option one is chosen you get to add a new student into the "Students" vector
-	if (input == 1)
+	while (true)
 	{
-		std::string name = "";
-		int age = 0;
+		//Prints out start menu
+		std::cout << "School System" << "\n" << "To add a student, Press: 1" << "\n" << "To delete a student, Press: 2" << "\n" << "To search student, Press: 3" << "\n";
+		std::cout << "click to continue";
 
-		std::cout << "Write the students name: " << "\n";
-		std::cin >> name;
-		std::cout << "Now write the students age: " << "\n";
-		std::cin >> age;
+		//std::cin.ignore('\n', 1);
+		std::cin.get();
 
-		AddStudent(name, age);
+		//int to take the input to know where to go from the start menu
+		int input;
+		std::cin >> input;
 
-		for (auto i : students) 
+		//if option one is chosen you get to add a new student into the "Students" vector
+		if (input == 1)
 		{
-			std::cout << i.name << "\n";
-			std::cout << i.age << "\n";
+			system("CLS");
+			std::string name = "";
+			int age = 0;
+
+			std::cout << "Write the students name: " << "\n";
+			std::cin >> name;
+			system("CLS");
+			std::cout << "Now write the students age: " << "\n";
+			std::cin >> age;
+			system("CLS");
+
+			AddStudent(name, age);
+
+			for (auto i : students)
+			{
+				std::cout << i.name << "\n";
+				std::cout << i.age << "\n\n";
+				
+			}
+			system("CLS");
 		}
-		 
+		//in progress
+		else if (input == 2)
+		{
+			system("CLS");
+			std::cout << "Byebye";
+		}
+		//in progress
+		else if (input == 3)
+		{
+			system("CLS");
+			std::cout << "I have no idea what to do now";
+		}
 	}
-	//in progress
-	else if (input == 2)
-	{
-		std::cout << "Byebye";
 	}
-}
+	
 
 //Add student function
 void SchoolSystem::AddStudent(std::string name, int age)
@@ -43,6 +62,7 @@ void SchoolSystem::AddStudent(std::string name, int age)
 	student.name = name;
 	student.age = age;
 	students.push_back(student);
+
 }
 
 void SchoolSystem::RemoveStudent()
